a1301aa8dd470559730956472b5c276242b04d19
kyathi-lekkala <klekkala@gitam.in>
2024-12-20 11:07:18 +0530

    Update q1.c

--- q1.c
+++ q1.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int arr[] = {1, 2, 3, 4, 3, 5}; // Example array
-    int key = 3;                    // Key to remove
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int count = 0;
+    int n, key, count = 0;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the key value to check against: ");
+    scanf("%d", &key);
 
     for (int i = 0; i < n; i++) {
         if (arr[i] != key) {
@@ -13,5 +23,6 @@ int main() {
     }
 
     printf("Number of values not equal to %d: %d\n", key, count);
+
     return 0;
 }
