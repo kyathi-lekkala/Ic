bc834bad92580bae56ada5e65b95c0a4aa2ade6c
kyathi-lekkala <klekkala@gitam.in>
2024-12-20 10:29:59 +0530

    Create q1.c

---
+++ q1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 3, 5}; // Example array
+    int key = 3;                    // Key to remove
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != key) {
+            count++;
+        }
+    }
+
+    printf("Number of values not equal to %d: %d\n", key, count);
+    return 0;
+}
